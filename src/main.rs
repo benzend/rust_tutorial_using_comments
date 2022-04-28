1116370325aec522f5f5563881eb8ad7872c2324
Benjamin Scott <benjamin.scottt.dev@gmail.com>
2022-04-27 19:38:07 -0600

    chore: added comments

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::io;
 fn main() {
     loop {
         // * `mut` allows mutation
+        // * String::new(); sets you up with an empty string
         let mut range_start = String::new();
         let mut range_end = String::new();
     
@@ -39,8 +40,8 @@ fn main() {
     
         println!("Now processing...");
 
-        // * Rust doesn't handle ranges that go from smaller to larger e.g. 5..1
-        // * the only way to handle this is by converting it to 1..5 and running .rev() on it
+        // * Rust doesn't handle ranges that go from larger to smaller e.g. 5..1
+        // * the only way to handle this is by converting it to 1..5 and calling .rev()
         if range_start > range_end {
             for n in (range_end..range_start + 1).rev() {
                 println!("{}", n);
@@ -56,5 +57,7 @@ fn main() {
 
 fn good_bye() {
     println!("Goodbye");
+
+    // * Exit the application
     std::process::exit(0x0100);
 }
